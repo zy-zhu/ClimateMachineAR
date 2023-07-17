c08f6e42253ce554f6d594f5e0912227cad7b80b
ZoeyZHUUUU <50037741+zy-zhu@users.noreply.github.com>
2023-07-17 11:02:13 -0700

    push latest

--- click.js
+++ click.js
@@ -97,7 +97,7 @@ function ClickNext0() {
     var numberfill = document.getElementById('number')
     if (numberfill.value != '') {
         // document.getElementById('scene01').style.display = "flex";
-        document.getElementById('menu').style.display = 'flex';
+        // document.getElementById('menu').style.display = 'flex';
         document.getElementById('scene0').style.display = 'none';
         document.getElementById('arscene1').style.display='flex';
         console.log('ok to go')
